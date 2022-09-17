d2db7dcf8e72755165e1a4838bb24df440aaeffc
Mayankv20 <62798875+Mayankv20@users.noreply.github.com>
2022-09-18 06:34:51 +1000

    Add files via upload

---
+++ week1_4.js
@@ -0,0 +1,15 @@
+let z
+let y
+let x
+let w
+let v
+let u;
+let t
+z = 9;
+y = 12;
+x = 13;
+u = 11;
+t = 8;
+v = 9;
+w = (z + y + x + u + v + t)/5;
+document.getElementById("average_fruits").innerHTML = w;
